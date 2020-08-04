9f6a7e1d397bbdf0e8dd6508ecbf4c091a81bf81
BogdanXin <499295338@qq.com>
2020-08-04 19:26:11 +0800

    Initial commit

---
+++ src/main/java/com/softlab/mutualsystem/MutualSystemApplication.java
@@ -0,0 +1,13 @@
+package com.softlab.mutualsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutualSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softlab/mutualsystem/MutualSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softlab.mutualsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
